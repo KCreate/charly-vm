207270fa0844587b5273b322a601f5d832461bfb
Leonard Schuetz <leni.schuetz@me.com>
2018-02-09 14:55:06 +0100

    Removed trailing whitespaces from assembly examples

--- examples/asm-examples/generators.asm
+++ examples/asm-examples/generators.asm
@@ -1,8 +1,8 @@
 Disassembly of block at 0x175c310
-    0x000001761c10: nop 
+    0x000001761c10: nop
 *-- 0x000001761c11: putfunction @"__CHARLY_MODULE_FUNC", 0x18, true, 1, 4
-|   0x000001761c27: halt 
-*-> 0x000001761c28: nop 
+|   0x000001761c27: halt
+*-> 0x000001761c28: nop
 *-- 0x000001761c29: putfunction @"create_generator", 0xcd, false, 0, 2
 |   0x000001761c3f: setlocal 2, 0
 |   0x000001761c48: readlocal 2, 0
@@ -12,38 +12,38 @@ Disassembly of block at 0x175c310
 |   0x000001761c68: readlocal 3, 0
 |   0x000001761c71: call 0
 |   0x000001761c76: call 1
-|   0x000001761c7b: pop 
+|   0x000001761c7b: pop
 |   0x000001761c7c: readlocal 13, 1
 |   0x000001761c85: readlocal 3, 0
 |   0x000001761c8e: call 0
 |   0x000001761c93: call 1
-|   0x000001761c98: pop 
+|   0x000001761c98: pop
 |   0x000001761c99: readlocal 13, 1
 |   0x000001761ca2: readlocal 3, 0
 |   0x000001761cab: call 0
 |   0x000001761cb0: call 1
-|   0x000001761cb5: pop 
+|   0x000001761cb5: pop
 |   0x000001761cb6: readlocal 13, 1
 |   0x000001761cbf: readlocal 3, 0
 |   0x000001761cc8: call 0
 |   0x000001761ccd: call 1
-|   0x000001761cd2: pop 
+|   0x000001761cd2: pop
 |   0x000001761cd3: readlocal 1, 0
-|   0x000001761cdc: return 
-*-> 0x000001761cdd: nop 
+|   0x000001761cdc: return
+*-> 0x000001761cdd: nop
 *-- 0x000001761cde: putgenerator @"create_generator", 0xdc
-|   0x000001761ceb: return 
-*-> 0x000001761cec: nop 
+|   0x000001761ceb: return
+*-> 0x000001761cec: nop
     0x000001761ced: putvalue 0x7ffc000000000000
     0x000001761cf6: setlocal 1, 0
-*-> 0x000001761cff: nop 
+*-> 0x000001761cff: nop
 |   0x000001761d00: readlocal 1, 0
-|   0x000001761d09: yield 
-|   0x000001761d0a: pop 
+|   0x000001761d09: yield
+|   0x000001761d0a: pop
 |   0x000001761d0b: readlocal 1, 0
 |   0x000001761d14: putvalue 0x7ffc000000000001
-|   0x000001761d1d: add 
+|   0x000001761d1d: add
 |   0x000001761d1e: setlocal 1, 0
 *-- 0x000001761d27: branch 0x0000ef
     0x000001761d2c: putvalue 0x7ffb000000000000
-    0x000001761d35: return 
+    0x000001761d35: return
--- examples/asm-examples/ifelse.asm
+++ examples/asm-examples/ifelse.asm
@@ -1,35 +1,35 @@
 Disassembly of block at 0x129cb60
-          0x00000129cb80: nop 
+          0x00000129cb80: nop
       *-- 0x00000129cb81: putfunction @"__CHARLY_MODULE_FUNC", 0x18, true, 1, 3
-      |   0x00000129cb97: halt 
-      *-> 0x00000129cb98: nop 
+      |   0x00000129cb97: halt
+      *-> 0x00000129cb98: nop
           0x00000129cb99: putvalue 0x7ffc000000000019
           0x00000129cba2: setlocal 2, 0
           0x00000129cbab: readlocal 2, 0
           0x00000129cbb4: putvalue 0x7ffc000000000014
-          0x00000129cbbd: gt 
+          0x00000129cbbd: gt
       *-- 0x00000129cbbe: branchunless 0x000061
-      |   0x00000129cbc3: nop 
+      |   0x00000129cbc3: nop
       |   0x00000129cbc4: readlocal 13, 1
       |   0x00000129cbcd: putstring "num is bigger than 20"
       |   0x00000129cbd6: call 1
-      |   0x00000129cbdb: pop 
+      |   0x00000129cbdb: pop
 *-----+-- 0x00000129cbdc: branch 0x0000b1
-|     *-> 0x00000129cbe1: nop 
+|     *-> 0x00000129cbe1: nop
 |         0x00000129cbe2: readlocal 2, 0
 |         0x00000129cbeb: putvalue 0x7ffc00000000000a
-|         0x00000129cbf4: gt 
+|         0x00000129cbf4: gt
 |  *----- 0x00000129cbf5: branchunless 0x000098
-|  |      0x00000129cbfa: nop 
+|  |      0x00000129cbfa: nop
 |  |      0x00000129cbfb: readlocal 13, 1
 |  |      0x00000129cc04: putstring "num is bigger than 10"
 |  |      0x00000129cc0d: call 1
-|  |      0x00000129cc12: pop 
+|  |      0x00000129cc12: pop
 |  |  *-- 0x00000129cc13: branch 0x0000b1
-|  *--+-> 0x00000129cc18: nop 
+|  *--+-> 0x00000129cc18: nop
 |     |   0x00000129cc19: readlocal 13, 1
 |     |   0x00000129cc22: putstring "num is smaller than 10"
 |     |   0x00000129cc2b: call 1
-|     |   0x00000129cc30: pop 
+|     |   0x00000129cc30: pop
 *-----*-> 0x00000129cc31: readlocal 1, 0
-          0x00000129cc3a: return 
+          0x00000129cc3a: return
