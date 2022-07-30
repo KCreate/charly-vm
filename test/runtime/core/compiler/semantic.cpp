97eb2ac633b8093e70a1816a75e4b43ec45e948e
Leonard Schütz <leni.schuetz@me.com>
2022-07-30 19:59:31 +0200

    Removed now failing test case
    Associated check removed in fc49b1546cdfcb5843b3a3fd39a9733a37ba9f62

--- test/runtime/core/compiler/semantic.cpp
+++ test/runtime/core/compiler/semantic.cpp
@@ -203,10 +203,6 @@ CATCH_TEST_CASE("Semantic") {
     COMPILE_OK("class A { func constructor {} }")
   }
 
-  CATCH_SECTION("checks for excess calls to parent constructor in subclasses") {
-    COMPILE_ERROR("class A extends B { func constructor { super() super() } }", "constructor of class 'A' may only contain a single call to the super constructor")
-  }
-
   CATCH_SECTION("checks for illegal calls to parent constructor in non inheriting classes") {
     COMPILE_ERROR("class A { func constructor { super() } }", "call to super not allowed in constructor of non-inheriting class 'A'")
   }
