5cdd77af6e1762b64be09b500c6cedab09ebe8d7
Leonard Schütz <leni.schuetz@me.com>
2022-09-12 00:21:15 +0200

    Added missing load/store emitters for self, farself

--- src/charly/core/compiler/codegenerator.cpp
+++ src/charly/core/compiler/codegenerator.cpp
@@ -166,6 +166,14 @@ ref<ir::IRInstruction> CodeGenerator::generate_load(const ValueLocation& locatio
     case ValueLocation::Type::FarFrame: {
       return m_builder.emit_loadfar(location.as.far_frame.depth, location.as.far_frame.index);
     }
+    case ValueLocation::Type::Self: {
+      m_builder.emit_loadself();
+      return m_builder.emit_loadattrsym(m_builder.register_string(location.name));
+    }
+    case ValueLocation::Type::FarSelf: {
+      m_builder.emit_loadfarself(location.as.far_self.depth);
+      return m_builder.emit_loadattrsym(m_builder.register_string(location.name));
+    }
     case ValueLocation::Type::Global: {
       return m_builder.emit_loadglobal(m_builder.register_string(location.name));
     }
@@ -189,6 +197,16 @@ ref<ir::IRInstruction> CodeGenerator::generate_store(const ValueLocation& locati
     }
     case ValueLocation::Type::Global: {
       return m_builder.emit_setglobal(m_builder.register_string(location.name));
+    }
+    case ValueLocation::Type::Self: {
+      m_builder.emit_loadself();
+      m_builder.emit_swap();
+      return m_builder.emit_setattrsym(m_builder.register_string(location.name));
+    }
+    case ValueLocation::Type::FarSelf: {
+      m_builder.emit_loadfarself(location.as.far_self.depth);
+      m_builder.emit_swap();
+      return m_builder.emit_setattrsym(m_builder.register_string(location.name));
     }
     default: UNREACHABLE();
   }
