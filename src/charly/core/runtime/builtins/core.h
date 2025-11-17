55394769c8e084ec74cb67a9f272f05e7efa91ff
Harto <harto@harto.dev>
2025-11-18 00:21:52 +0100

    Implement getenv as builtin function

--- src/charly/core/runtime/builtins/core.h
+++ src/charly/core/runtime/builtins/core.h
@@ -46,6 +46,7 @@ void initialize(Thread* thread);
   V(core, getsteadytimestamp, 0)      \
   V(core, getsteadytimestampmicro, 0) \
   V(core, readfile, 1)                \
+  V(core, getenv, 1)                  \
   V(core, compile, 2)
 DEF_BUILTIN_CORE(DEFINE_BUILTIN_METHOD_DECLARATIONS)
 
